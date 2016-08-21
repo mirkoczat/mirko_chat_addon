18a5fbfff330524f4f4d139c00e63757ffdfaa3c
mirkoczat <mirkoczat@users.noreply.github.com>
2016-08-21 17:25:11 +0200

    0.0.5 Floating window

--- firefox/main.js
+++ firefox/main.js
@@ -1,27 +1,10 @@
 'use strict';
-jQuery('.mainnav').append('<li><a href="https://mirkoczat.pl/">MirkoCzat</a></li>')
-jQuery('#site .grid-right').prepend('<div class="r-block"><h4>MirkoCzat</h4><div id="mirkoczat" class="fix-tagline"></div><div id="mirkoczat_embedded" class="fix-tagline"></div></div>')
-var mirkoczat = {
-  clear: function() {
-    jQuery('#mirkoczat').html("");
-  }
-  ,update: function() {
-    jQuery.getJSON('https://mirkoczat.pl/rooms.json', function(data) {
-      mirkoczat.clear();
-      data.rooms.map(function(room) {
-        mirkoczat.add(room.name, room.count)
-      });
-    })
-  }
-  ,add: function(tag, count) {
-    var a = jQuery('<a class="tag create"></a>').attr('href', 'https://mirkoczat.pl/t/' + tag).text('#' + tag + ' (' + count + ')');
-    jQuery('#mirkoczat').append(a);
-  }
-}
-jQuery('#newEntriesCounter').on('click', mirkoczat.update);
+jQuery('.mainnav').append('<li><a href="https://mirkoczat.pl/t/indywidualny">MirkoCzat</a></li>')
+jQuery('body').append('\
+<div class="r-block" style="position: fixed; z-index: 10; top: 55px; right: 5px; width: 324px; height: 620px; border: 5px solid #1c1c1c; border-radius: 0px">\
+  <div id="mirkoczat_embedded" class="fix-tagline"></div>\
+</div>');
 /* Displaying chat */
-var tag = $('#input-tag').val();
-if (!tag) { tag = "wykop" }
+var tag = jQuery('#input-tag').val();
 var iframe = jQuery('<iframe height="600" width="100%"></iframe>').attr('src', 'https://mirkoczat.pl/embed_t/' + tag);
-$('#mirkoczat_embedded').html(iframe);
-mirkoczat.update()
+jQuery('#mirkoczat_embedded').html(iframe);
